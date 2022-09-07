8bb233387325de22e8a61eb9a9a1f7a41d78445a
gnilk <gnilk@noreply.com>
2022-09-07 11:37:19 +0100

    FIX: Fiddling with logger configuration

--- src/testrunner/tests/test_module_nix.cpp
+++ src/testrunner/tests/test_module_nix.cpp
@@ -3,6 +3,7 @@
 //
 
 // using dev-version, instead of install version... self-hosting..
+#include "../config.h"
 #include "../testinterface.h"
 #include "../dynlib.h"
 #include "../dynlib_linux.h"
@@ -12,12 +13,15 @@ DLL_EXPORT int test_module(ITesting *t);
 DLL_EXPORT int test_module_scan(ITesting *t);
 DLL_EXPORT int test_module_symbol(ITesting *t);
 DLL_EXPORT int test_module_copysym(ITesting *t);
-DLL_EXPORT int test_glob1(ITesting *t);
-DLL_EXPORT int test_glob2(ITesting *t);
 }
 
 
 DLL_EXPORT int test_module(ITesting *t) {
+    // Since we are testing the internals we will be linking against
+    // the runner configuration instance and affect the global logger etc..
+    Config::Instance();
+    gnilk::Logger::SetAllSinkDebugLevel(gnilk::Logger::kMCError);
+
     return kTR_Pass;
 }
 
@@ -44,6 +48,7 @@ DLL_EXPORT int test_module_symbol(ITesting *t) {
 #else
     TR_ASSERT(t, modloader.Scan("lib/libtrun_utests.so"));
 #endif
+
     TR_ASSERT(t, res);
     // We should find this...
     TR_ASSERT(t, modloader.FindExportedSymbol("test_module_symbol"));
@@ -63,12 +68,5 @@ DLL_EXPORT int test_module_copysym(ITesting *t) {
 #endif
     TR_ASSERT(t, res == true);
     TR_ASSERT(t, modloader.Exports().size() > 0);
-    return kTR_Pass;
-}
-// These are global functions - but they end up as modules
-DLL_EXPORT int test_glob1(ITesting *t) {
-    return kTR_Pass;
-}
-DLL_EXPORT int test_glob2(ITesting *t) {
     return kTR_Pass;
 }
