7b68156fcd9bda78f1e7a9390a9d08bf84989d8f
gnilk <gnilk@noreply.com>
2022-09-10 14:15:48 +0100

    CHG: Consolidated timer to one class for all platforms

--- src/testrunner/timer.cpp
+++ src/testrunner/timer.cpp
@@ -2,9 +2,8 @@
  File    : timer.cpp
  Author  : FKling
  Version : -
- Orginal : 2018-10-18
+ Orginal : 2020-01-08
  Descr   : Simple timer to measure duration between Reset/Sample call's
-
 
  Part of testrunner
  BSD3 License!
@@ -18,15 +17,18 @@
  
  
  \History
- - 2018.10.18, FKling, Implementation
+ - 2020.01.08, FKling, Implementation
  
  ---------------------------------------------------------------------------*/
 
+#ifdef WIN32
 #include <windows.h>
 #include <winnt.h>
-
+#endif
 #include "timer.h"
+#include <chrono>
 
+typedef std::chrono::steady_clock Clock;
 
 Timer::Timer() {
     Reset();
@@ -36,21 +38,21 @@ Timer::Timer() {
 // Restet starting point for clock
 //
 void Timer::Reset() {
-
-    QueryPerformanceFrequency(&Frequency); 
-    QueryPerformanceCounter(&StartingTime);
+    time_start = Clock::now();
 }
 
 //
 // Sample the time between last reset and now, return as double in seconds
+// Note: This could be a one-liner but I had to debug and decided to leave it like this as it makes it easier to read..
 //
 double Timer::Sample() {
-    double ret;
+    double ret = 0.0;
 
+    auto elapsed = Clock::now();
+    auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(elapsed - time_start).count();
 
-    QueryPerformanceCounter(&EndingTime);
-    ret = EndingTime.QuadPart - StartingTime.QuadPart;
-    ret /= Frequency.QuadPart;
+    ret = ms / 1000.0f;
+  
 
     return ret;
 }
