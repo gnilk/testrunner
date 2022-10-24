96c578bbf9056edd1880b25580ad2683b51f4e0f
gnilk <1664069+gnilk@users.noreply.github.com>
2022-10-24 20:48:39 +0200

    FIX: Cleanup test-cases FIX: Multiple dependency test-case

--- src/testrunner/tests/test_depends.cpp
+++ src/testrunner/tests/test_depends.cpp
@@ -11,9 +11,7 @@ DLL_EXPORT int test_depends_func2(ITesting *t);
 DLL_EXPORT int test_depends_func3(ITesting *t);
 }
 DLL_EXPORT int test_depends(ITesting *t) {
-    t->CaseDepends("func3", "func3");
-    t->CaseDepends("func2", "func1");
-    t->CaseDepends("func1", "func3");
+    t->CaseDepends("func1", "func3,func2");
     return kTR_Pass;
 }
 DLL_EXPORT int test_depends_func1(ITesting *t) {
