bf5e31e04d08f9538f092a4034921434e0345215
gnilk <1664069+gnilk@users.noreply.github.com>
2022-10-24 21:01:03 +0200

    FIX: Added multiple dependency list to unit tests

--- src/testrunner/tests/test_depends.cpp
+++ src/testrunner/tests/test_depends.cpp
@@ -9,9 +9,14 @@ DLL_EXPORT int test_depends(ITesting *t);
 DLL_EXPORT int test_depends_func1(ITesting *t);
 DLL_EXPORT int test_depends_func2(ITesting *t);
 DLL_EXPORT int test_depends_func3(ITesting *t);
+DLL_EXPORT int test_depends_readDb(ITesting *t);
+DLL_EXPORT int test_depends_writeDb(ITesting *t);
+DLL_EXPORT int test_depends_connectDb(ITesting *t);
 }
 DLL_EXPORT int test_depends(ITesting *t) {
     t->CaseDepends("func1", "func3,func2");
+    t->CaseDepends("readDb", "connectDb,writeDb");
+    t->CaseDepends("writeDb", "connectDb");
     return kTR_Pass;
 }
 DLL_EXPORT int test_depends_func1(ITesting *t) {
@@ -21,5 +26,14 @@ DLL_EXPORT int test_depends_func2(ITesting *t) {
     return kTR_Pass;
 }
 DLL_EXPORT int test_depends_func3(ITesting *t) {
+    return kTR_Pass;
+}
+DLL_EXPORT int test_depends_readDb(ITesting *t) {
+    return kTR_Pass;
+}
+DLL_EXPORT int test_depends_writeDb(ITesting *t) {
+    return kTR_Pass;
+}
+DLL_EXPORT int test_depends_connectDb(ITesting *t) {
     return kTR_Pass;
 }
