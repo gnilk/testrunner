e47d5b2582c5711865267babc5586d7e31d33777
gnilk <1664069+gnilk@users.noreply.github.com>
2023-11-21 09:56:17 +0100

    Bumped version number

--- src/testrunner/config.cpp
+++ src/testrunner/config.cpp
@@ -44,7 +44,7 @@ Config::Config() {
     inputs.push_back(".");    // Search current directory
     modules.push_back("-");
     testcases.push_back("-");
-    version = "1.2-Dev";
+    version = "1.3-Dev";
     description = "C/C++ Unit Test Runner";
     mainFuncName = "main";
     exitFuncName = "exit";
