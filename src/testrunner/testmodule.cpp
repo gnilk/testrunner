cfdc824b6ba6a19f0eaf4b93c06f188657dbbd17
gnilk <1664069+gnilk@users.noreply.github.com>
2023-11-26 09:56:57 +0100

    CHG: Logic for executing module matches now use the strutil function 'caseMatch'

--- src/testrunner/testmodule.cpp
+++ src/testrunner/testmodule.cpp
@@ -18,24 +18,7 @@
 using namespace trun;
 
 bool TestModule::ShouldExecute() const {
-    int executeFlag = 0;
-
-    for (auto m: Config::Instance()->modules) {
-        if (m == "-") {
-            executeFlag = 1;
-            continue;;
-        }
-        auto isMatch = trun::match(name, m);
-        if ((isMatch) && (m[0]!='!')) {
-            executeFlag = 1;
-            goto leave;
-        }
-        if (!isMatch) {
-            executeFlag = 0;
-        }
-    }
-leave:
-    return (executeFlag==1);
+    return caseMatch(name, Config::Instance()->modules);
 }
 
 TestFunc *TestModule::TestCaseFromName(const std::string &caseName) const {
