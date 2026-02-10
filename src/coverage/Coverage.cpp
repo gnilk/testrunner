4be390def9ecebf497cc7e5617b7db990b514eac
gnilk <1664069+gnilk@users.noreply.github.com>
2026-02-10 07:52:43 +0100

    NEW: Added simple reporting method

--- src/coverage/Coverage.cpp
+++ src/coverage/Coverage.cpp
@@ -389,7 +389,7 @@ void CoverageRunner::End() {
 // Generate the coverage report
 // FIXME: extend this - we should drop a file with all details
 void CoverageRunner::Report() {
-    breakpointManager.Report();
+    //breakpointManager.Report();
     // // TEMPORARY
     // size_t linestested = 0;
     // for (auto &bp : breakpoints) {
@@ -401,4 +401,30 @@ void CoverageRunner::Report() {
     // }
     // auto factor = (float)linestested / (float)breakpoints.size();
     // printf("coverage=%f = %d%%\n", factor, (int)(100 * factor));
+    auto coverageData = breakpointManager.ComputeCoverage();
+
+
+    std::sort(coverageData.begin(),coverageData.end(),[](auto &a, auto &b) {
+        return (a.functionCoverage > b.functionCoverage);
+    });
+
+    // NEED MUCH ELABORATE REPORTING!
+    static uint32_t ranges[]={
+        100,80,60,40,20,0,0,
+    };
+    uint32_t idxCurrentRange = 0;
+    for (auto &cov : coverageData) {
+        if (cov.percentageCoverage <= ranges[idxCurrentRange]) {
+            printf("%d\n",cov.percentageCoverage);
+            while (cov.percentageCoverage <= ranges[idxCurrentRange]) {
+                idxCurrentRange++;
+            }
+        }
+        printf("   %s - Coverage: %d%% (%.3f) (hits: %zu, bp:%zu) \n",
+            cov.ptrFunction->symbol.GetName(),
+            cov.percentageCoverage,
+            cov.functionCoverage,
+            cov.nHits,
+            cov.nBreakpoints);
+    }
 }
