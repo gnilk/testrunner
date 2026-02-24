9278a8bdc4e38b37c141dc90f802b49c99ba8245
gnilk <1664069+gnilk@users.noreply.github.com>
2026-02-24 11:35:41 +0100

    NEW: Unit test in C to test
    This just verify that it compiles - the test is empty.

---
+++ src/testrunner/tests/test_fromc.c
@@ -0,0 +1,12 @@
+//
+// Created by gnilk on 24.02.26.
+//
+#include "ext_testinterface/testinterface.h"
+
+int test_fromc(ITesting *t) {
+    return kTR_Pass;
+}
+
+int test_fromc_case(ITesting *t) {
+    return kTR_Pass;
+}
